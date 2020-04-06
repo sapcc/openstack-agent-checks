2ec4b66b7e5ad86a4a9b85f4e6a19b2cce38bfdb
Andrew Karpow <andrew.karpow@sap.com>
2020-04-06 11:55:18 +0200

    use data_files for setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="openstack-agent-checks",
     version="0.1",
-    scripts=[
+    data_files=[('bin', [
         'cmd/neutron-agent-liveness/neutron-agent-liveness', 
         'cmd/neutron-dhcp-readiness/neutron-dhcp-readiness',
         'cmd/neutron-linuxbridge-readiness/neutron-linuxbridge-readiness'
-    ],
+    ])],
     exclude_package_data={'': ['tests']},
 )
