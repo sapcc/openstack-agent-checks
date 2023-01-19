a6438bc7fbb779d6c3ad250ab5638b462d967bbf
Sebastian Wagner <sebastian.wagner02@sap.com>
2023-01-19 16:44:56 +0100

    Add prometheus exporter to run as a sidecar
    Instead of running the check if all bridges/netns have been materialized
    as liveness/readyness probe, the idea of this exporter is to simply
    export if a bridge/netns is present or not. This can than be compared
    with other metrics that indicate the host <> network agent mapping.

--- Makefile
+++ Makefile
@@ -5,6 +5,13 @@ all:
 	done
 
 clean:
+	if [ -d "bin" ]; then rm -rf bin; fi; \
 	for dir in cmd/*; do \
 		[[ -f $$dir/Makefile ]] && echo Entering into "$$dir" && $(MAKE) -C "$$dir" clean; \
+	done
+
+bin: all
+	if ! [ -d "bin" ]; then mkdir bin; fi; \
+	for dir in cmd/*; do \
+		mv $$dir/$$(basename $$dir) bin/; \
 	done
---
+++ cmd/neutron-linuxbridge-dhcp-exporter/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	listenAddress  = kingpin.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":9116").String()
+	updateInterval = kingpin.Flag("update-interval", "Interval of checking for new linux bridges").Default("30").Int()
+	setCurrentTime = kingpin.Flag("set-current-time", "Instead of binary gauges, set the time as timeticks").Default("false").Bool()
+	bridgesPath    = kingpin.Flag("bridges-path", "Virtual filesystem path exposing kernel network interfaces").Default("/sys/class/net").String()
+	netNsPath      = kingpin.Flag("netns-path", "Virtual filesystem path network namespaces").Default("/var/run/netns/").String()
+)
+
+func loopFileSystem(hostname string, path string, prefix string, label string, interval int, setCurrentTime bool, m *prometheus.GaugeVec, sig chan os.Signal) {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			log.Printf("Re-reading %s\n", path)
+			files, err := ioutil.ReadDir(path)
+			if err != nil {
+				log.Fatalf("Failed reading from %s: %s", path, err)
+			}
+			// reset the vector so old/removed bridges do not appear in the scrape
+			m.Reset()
+			for _, f := range files {
+				if strings.HasPrefix(f.Name(), prefix) {
+					label := prometheus.Labels{"hostname": hostname, label: strings.TrimPrefix(f.Name(), prefix)}
+					if setCurrentTime {
+						m.With(label).SetToCurrentTime()
+					} else {
+						m.With(label).Set(1)
+					}
+				}
+			}
+		case received := <-sig:
+			log.Printf("Shutting down %s collector", path)
+			ticker.Stop()
+			// add the signal again so the other goroutine also gets it and stops
+			sig <- received
+			return
+		}
+	}
+}
+
+func main() {
+
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal("Unable to determine hostame", err)
+		os.Exit(1)
+	}
+
+	reg := prometheus.NewRegistry()
+	bridge_metric := prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "linux_bridge_present", Help: "Indicates if a bridge is present on the agent"}, []string{"hostname", "bridge"})
+	netns_metric := prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "netns_present", Help: "Indicates if a network namespace is present on the agent"}, []string{"hostname", "netns"})
+	reg.MustRegister(bridge_metric)
+	reg.MustRegister(netns_metric)
+
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	server := &http.Server{Addr: *listenAddress}
+
+	go func() {
+		log.Printf("Starting prometheus http server on %s\n", *listenAddress)
+		if err := server.ListenAndServe(); err != nil && err.Error() != "http: Server closed" {
+			log.Fatalf("Error starting prometheus http server: %s", err)
+			os.Exit(-1)
+		}
+	}()
+
+	sig := make(chan os.Signal)
+	loopSig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	signal.Notify(loopSig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	go loopFileSystem(hostname, *bridgesPath, "brq", "bridge", *updateInterval, *setCurrentTime, bridge_metric, loopSig)
+	go loopFileSystem(hostname, *netNsPath, "qdhcp-", "netns", *updateInterval, *setCurrentTime, netns_metric, loopSig)
+
+	<-sig
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("http server stopped")
+}
